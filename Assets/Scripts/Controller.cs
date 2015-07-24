1c23fe889825d2eb46d7bfda26d69abfdf25446c
XIIIvs <xiii_1994@wp.pl>
2015-07-24 22:35:29 +0200

    Cos dziala ;)
    Zmiana wyswietlania testu przez kontroler gry.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public Text frustrationLevel;
+    public int levelFrustration;
+
+	void Start () {
+        GameObject textFrustrationFinding = GameObject.FindWithTag("Frustration");
+        if(textFrustrationFinding != null)
+        {
+            Debug.Log("LOAD :: Zaladowane Frustration Object");
+            frustrationLevel = textFrustrationFinding.GetComponent<Text>();
+            frustrationLevel.text = "Frustration: " + levelFrustration;
+        }
+        levelFrustration = 0;
+	}
+	
+	void Update () {
+	
+	}
+}
