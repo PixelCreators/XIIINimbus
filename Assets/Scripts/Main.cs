8e841da7d812d7948d150a36c6eada5023da9068
Weronika Sieinska <weronikasieinska@wp.pl>
2015-07-24 20:31:20 +0200

    second commit, first scripts and assets

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+
+    public int frustrationLevel = 0;
+    public Text frustrationLevelText;
+
+    void Start () {
+        frustrationLevelText.text = "Level of frustration: " + frustrationLevel;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
