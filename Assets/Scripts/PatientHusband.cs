2163057605a7d84cba70079fbde14c8bfb638c4b
Weronika Sieinska <weronikasieinska@wp.pl>
2015-07-25 17:08:23 +0200

    tempScene with smoke button

---
+++ Assets/Scripts/PatientHusband.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatientHusband : MonoBehaviour
+{	
+	// Update is called once per frame
+
+    public Controller myController; //obiekt kontrolera
+    public int deltaFrustration = 50;    //roznica, o jaka zmienia sie frustracja w wyniku eventu lub akcji meza
+
+    void Start()
+    {
+        
+    }
+
+    public void Smoke()
+    {
+        myController.levelFrustration = myController.levelFrustration - deltaFrustration;
+    }
+
+	void Update ()
+    {
+	
+	}
+}
