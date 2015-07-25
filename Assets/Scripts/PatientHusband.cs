37ed72a2201ad40bd1b5877d096d45563a6a9c24
Weronika Sieinska <weronikasieinska@wp.pl>
2015-07-25 19:30:18 +0200

    action buttons added and working

--- Assets/Scripts/PatientHusband.cs
+++ Assets/Scripts/PatientHusband.cs
@@ -6,16 +6,121 @@ public class PatientHusband : MonoBehaviour
 	// Update is called once per frame
 
     public Controller myController; //obiekt kontrolera
-    public int deltaFrustration = 50;    //roznica, o jaka zmienia sie frustracja w wyniku eventu lub akcji meza
+    bool meditationUsed = false;
+    bool smokingUsed = false;
+    bool shoutingUsed = false;
+    bool drinkingUsed = false;
+    bool quoteUsed = false;
+    bool manipulationUsed = false;
+    bool justDoItUsed = false;
 
     void Start()
     {
-        
+        GameObject findController = GameObject.FindWithTag("GameController");
+        if (findController != null)
+        {
+            myController = findController.GetComponent<Controller>();
+        }
+    }
+
+
+    //ACTIONS:
+    public void Meditate()
+    {
+        if (meditationUsed == false)
+        {
+            if (myController.levelFrustration > 50)
+                myController.levelFrustration = myController.levelFrustration - 50;
+            else
+                myController.levelFrustration = 0;
+            meditationUsed = true;
+        }
+        else
+            return;
     }
 
     public void Smoke()
     {
-        myController.levelFrustration = myController.levelFrustration - deltaFrustration;
+        if (smokingUsed == false)
+        {
+            if (myController.levelFrustration > 50)
+                myController.levelFrustration = myController.levelFrustration - 50;
+            else
+                myController.levelFrustration = 0;
+            smokingUsed = true;
+        }
+        else
+            return;
+    }
+
+    public void Shout()
+    {
+        if (shoutingUsed == false)
+        {
+            if (myController.levelFrustration > 100)
+                myController.levelFrustration = myController.levelFrustration - 100;
+            else
+                myController.levelFrustration = 0;
+            shoutingUsed = true;
+        }
+        else
+            return;
+    }
+
+    public void DrinkVodka()
+    {
+        if (drinkingUsed == false)
+        {
+            if (myController.levelFrustration > 100)
+                myController.levelFrustration = myController.levelFrustration - 100;
+            else
+                myController.levelFrustration = 0;
+            drinkingUsed = true;
+        }
+        else
+            return;
+    }
+
+    public void QuoteYoda() //"Buy or do not buy. There is no try."
+    {
+        if (quoteUsed == false)
+        {
+            if (myController.levelFrustration > 150)
+                myController.levelFrustration = myController.levelFrustration - 150;
+            else
+                myController.levelFrustration = 0;
+            quoteUsed = true;
+        }
+        else
+            return;
+    }
+
+    public void Manipulate()
+    {
+        if (manipulationUsed == false)
+        {
+            if (myController.levelFrustration > 150)
+                myController.levelFrustration = myController.levelFrustration - 150;
+            else
+                myController.levelFrustration = 0;
+            manipulationUsed = true;
+        }
+        else
+            return;
+    }
+
+    public void JustDoIt()
+    {
+        if (justDoItUsed == false)
+        {
+            if (myController.levelFrustration > 200)
+                myController.levelFrustration = myController.levelFrustration - 200;
+            else
+                myController.levelFrustration = 0;
+            justDoItUsed = true;
+        }
+        else
+            return;
     }
 
 	void Update ()
